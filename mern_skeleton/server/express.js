fa94d3179702ec857445031f86498e57394270bf
graha <graham.squires3@gmail.com>
2021-08-28 16:48:57 -0400

    progress

---
+++ mern_skeleton/server/express.js
@@ -0,0 +1,21 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import compress from 'compression'
+import cors from 'cors'
+import helmet from "helmet"
+import Template from '../template.js'
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(compress);
+app.use(helmet);
+app.use(cors);
+/*... configures express ... */
+
+app.get('/', (req, res) => {
+    res.status(200).send(Template())
+})
+
+export default app
