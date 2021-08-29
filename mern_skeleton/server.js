fdae2b2a644acce309fe40aa59a85b209ce1bd00
graha <graham.squires3@gmail.com>
2021-08-29 10:56:25 -0400

    fixes

--- mern_skeleton/server.js
+++ mern_skeleton/server.js
@@ -1,21 +1,41 @@
-import config from "./config/config.js";
+// import config from "./config/config.js";
+// import app from './server/express.js'
+// import mongoose from 'mongoose'
+
+// mongoose.Promise = global.Promise
+// mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
+//     // useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     // useUnifiedTopology: true     
+// });
+
+// mongoose.connection.on('error', () => {
+//     throw new Error(`Unable to connect to database: {mongoUri}`)
+// });
+
+// app.listen(config.port, (err) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.info('Server started on port %s.', config.port)
+// });
+
+import config from './config/config.js'
 import app from './server/express.js'
 import mongoose from 'mongoose'
 
+// Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true     
-});
-
+mongoose.connect(config.mongoUri, 
+    // { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+    )
 mongoose.connection.on('error', () => {
-    throw new Error(`Unable to connect to database: {mongoUri}`)
-});
+  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
 
 app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.info('Server started on port %s.', config.port)
-});
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
+})
